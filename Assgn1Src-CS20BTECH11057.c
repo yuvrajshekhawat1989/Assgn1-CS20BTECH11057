d6f998d2c175f5077b2ee72d5abb32198eb85c75
yuvrajshekhawat1989 <80632526+yuvrajshekhawat1989@users.noreply.github.com>
2021-12-04 18:29:54 +0530

    Update Assgn1Src-CS20BTECH11057.c

--- Assgn1Src-CS20BTECH11057.c
+++ Assgn1Src-CS20BTECH11057.c
@@ -6,41 +6,41 @@
 int main(int argc, char **argv)
 {
     int num;
-    // checking if there is any argument passed or not and if not then exiting the program
+    /*checking if there is any argument passed or not and if not then exiting the program*/
     if (argc == 1)
     {
         printf("No command line argument is passed\n");
         exit(0);
     }
 
-    // converting the char into integer
+    /*If there is any argument the second element of array will be it, then we will use the atoi function to convert string into num*/
     num = atoi(argv[1]);
 
-    // checking - is the argument positive interger or not
+    /*checking if the argument is negative*/
     if (num <= 0)
     {
-        printf("\nThe number should be a positive interger.\n\n");
+        printf("The number must be a positive integer.\n");
         exit(0);
     }
-
+    
     if (fork() == 0)
     {
         // Child process
         while (num > 1)
         {
-            printf("%d ", num);
+            printf("%d", num);
             if (num % 2 == 0)
                 num = num / 2;
             else
                 num = 3 * num + 1;
         }
-        printf("1\n\n");
+        printf("1\n");
     }
     else
     {
-        // Parent process - waiting for child to complete
+        // Parent process may wait for child process to complete
         wait(NULL);
     }
-
+    
     return 0;
 }
